67f6b97405187e9e28a2e00d2a8588e64bae8810
BkzWorld <97360672+BkzWorld@users.noreply.github.com>
2024-12-02 20:15:02 +0530

    Second commit

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,117 +1,163 @@
-import { useState } from 'react';
-import Globe from 'react-globe.gl';
+import { useState } from "react";
+import Globe from "react-globe.gl";
 
-import Button from '../components/Button.jsx';
+import Button from "../components/Button.jsx";
 
 const About = () => {
-    const [hasCopied, setHasCopied] = useState(false);
+  const [hasCopied, setHasCopied] = useState(false);
 
-    const handleCopy = () => {
-        navigator.clipboard.writeText('bahnimaan.kashyap@gmail.com');
-        setHasCopied(true);
+  const handleCopy = () => {
+    navigator.clipboard.writeText("bahnimaan.kashyap@gmail.com");
+    setHasCopied(true);
 
-        setTimeout(() => {
-            setHasCopied(false);
-        }, 2000);
-    };
+    setTimeout(() => {
+      setHasCopied(false);
+    }, 2000);
+  };
 
-    return (
-        <section className="c-space my-20" id="about">
-            <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full">
-                <div className="col-span-1 xl:row-span-3">
-                    <div className="grid-container">
-                        <img src="assets/grid1.png" alt="grid-1" className="w-full sm:h-[276px] h-fit object-contain" />
+  return (
+    <section className="c-space my-20" id="about">
+      <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full">
+        <div className="col-span-1 xl:row-span-3">
+          <div className="grid-container">
+            <img
+              src="assets/grid1.png"
+              alt="grid-1"
+              className="w-full sm:h-[276px] h-fit object-contain"
+            />
 
-                        <div>
-                            <p className="grid-headtext">Hi, I’m Bahnimaan Kashyap</p>
-                            <p className="grid-subtext">
-                                With an year of experience, I have honed my skills in both frontend and backend dev, creating dynamic
-                                and responsive websites, now working on the field of Data Engineering and Analytics. Keen on gaining knowledge
-                                from anywhere and everywhere.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="col-span-1 xl:row-span-3">
-                    <div className="grid-container">
-                        <img src="assets/grid2.png" alt="grid-2" className="w-full sm:h-[276px] h-fit object-contain" />
-
-                        <div>
-                            <p className="grid-headtext">Tech Stack</p>
-                            <p className="grid-subtext">
-                                I specialize in a variety of languages, frameworks, and tools that allow me to build robust and scalable
-                                applications.
-                                <br></br>
-                                Currently pursuing Data Analytics and Engineering.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="col-span-1 xl:row-span-4">
-                    <div className="grid-container">
-                        <div className="rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center">
-                            <Globe
-                                height={326}
-                                width={326}
-                                backgroundColor="rgba(0, 0, 0, 0)"
-                                backgroundImageOpacity={0.5}
-                                showAtmosphere
-                                showGraticules
-                                globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
-                                bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
-                                labelsData={[{ lat: 12.9, lng: 77, text: 'I am here !', color: 'white', size: 30 }]}
-                            />
-                        </div>
-                        <div>
-                            <p className="grid-headtext">I’m very flexible with time zone communications & locations</p>
-                            <p className="grid-subtext mb150">I&apos;m based in Bengaluru, India and open to remote work worldwide.
-                            Zoom into south India on the Map.
-                            </p>
-                            <a href="https://www.linkedin.com/in/bahnimaan-kashyap/" target="_blank" rel="noopener noreferrer">
-                                <Button name="Contact me" isBeam containerClass="w-full mt-10"/>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="xl:col-span-2 xl:row-span-3">
-                <div className="grid-container">
-                        <img src="assets/grid3.png" alt="grid-3" className="w-full sm:h-[266px] h-fit object-contain"/>
-
-                        <div>
-                            <p className="grid-headtext">My Passion for Data Analysis and Problem solving</p>
-                            <p className="grid-subtext">
-                                I love understanding data, analyze, visualize and create insights,I also enjoy programming, with Data Structures and Algorithms.
-                                Both Data Visualization and Programming isn&apos;t just my
-                                profession—it&apos;s my passion. I enjoy exploring new technologies, and enhancing my skills.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="xl:col-span-1 xl:row-span-2">
-                    <div className="grid-container">
-                        <img
-                            src="assets/grid4.png"
-                            alt="grid-4"
-                            className="w-full md:h-[126px] sm:h-[276px] h-fit object-cover sm:object-top"
-                        />
-
-                        <div className="space-y-2">
-                            <p className="grid-subtext text-center">Contact me</p>
-                            <div className="copy-container" onClick={handleCopy}>
-                                <img src={hasCopied ? 'assets/tick.svg' : 'assets/copy.svg'} alt="copy" />
-                                <p className="lg:text-1.5xl md:text-xl font-medium text-gray_gradient text-white">bahnimaan.kashyap@gmail.com</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <div>
+              <p className="grid-headtext">Hi, I’m Bahnimaan Kashyap</p>
+              <p className="grid-subtext">
+                With an year of experience, I have honed my skills in both
+                frontend and backend dev, creating dynamic and responsive
+                websites, now working on the field of Data Engineering and
+                Analytics. Keen on gaining knowledge from anywhere and
+                everywhere.
+              </p>
             </div>
-        </section>
-    );
+          </div>
+        </div>
+
+        <div className="col-span-1 xl:row-span-3">
+          <div className="grid-container">
+            <img
+              src="assets/grid2.png"
+              alt="grid-2"
+              className="w-full sm:h-[276px] h-fit object-contain"
+            />
+
+            <div>
+              <p className="grid-headtext">Tech Stack</p>
+              <p className="grid-subtext">
+                I specialize in a variety of languages, frameworks, and tools
+                that allow me to build robust and scalable applications.
+                <br></br>
+                Currently pursuing Data Analytics and Engineering.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-span-1 xl:row-span-4">
+          <div className="grid-container">
+            <div className="rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center">
+              <Globe
+                height={326}
+                width={326}
+                backgroundColor="rgba(0, 0, 0, 0)"
+                backgroundImageOpacity={0.5}
+                showAtmosphere
+                showGraticules
+                globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+                bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+                labelsData={[
+                  {
+                    lat: 12.9,
+                    lng: 77,
+                    text: "I am here !",
+                    color: "white",
+                    size: 30,
+                  },
+                ]}
+              />
+            </div>
+            <div>
+              <p className="grid-headtext">
+                I’m very flexible with time zone communications & locations
+              </p>
+              <p className="grid-subtext mb150">
+                I&apos;m based in Bengaluru, India and open to remote work
+                worldwide. Zoom into south India on the Map.
+              </p>
+              <a
+                href="https://www.linkedin.com/in/bahnimaan-kashyap/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Button
+                  name="Contact me"
+                  isBeam
+                  containerClass="w-full mt-10"
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="xl:col-span-2 xl:row-span-3">
+          <div className="grid-container">
+            <img
+              src="assets/grid3.png"
+              alt="grid-3"
+              className="w-full sm:h-[266px] h-fit object-contain"
+            />
+
+            <div>
+              <p className="grid-headtext">
+                My Passion for Data Analysis and Problem solving
+              </p>
+              <p className="grid-subtext">
+                I love understanding data, analyze, visualize and create
+                insights,I also enjoy programming, with Data Structures and
+                Algorithms. Both Data Visualization and Programming isn&apos;t
+                just my profession—it&apos;s my passion. I enjoy exploring new
+                technologies, and enhancing my skills.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="xl:col-span-1 xl:row-span-2">
+          <div className="grid-container">
+            <img
+              src="assets/grid4.png"
+              alt="grid-4"
+              className="w-full md:h-[126px] sm:h-[276px] h-fit object-cover sm:object-top"
+            />
+
+            <div className="space-y-2">
+              <p className="grid-subtext text-center">Contact me</p>
+              <div className="copy-container" onClick={handleCopy}>
+                <img
+                  src={hasCopied ? "assets/tick.svg" : "assets/copy.svg"}
+                  alt="copy"
+                />
+                <p className="lg:text-1.5xl md:text-xl font-medium text-gray_gradient text-white">
+                  bahnimaan.kashyap@gmail.com
+                </p>
+              </div>
+              <div className="copy-container" onClick={handleCopy}>
+                <a href="/resume.pdf" download>
+                  <button className="download-btn lg:text-1.5xl md:text-xl mt:8 font-medium text-gray_gradient text-white">Download Resume 📜 </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
-
